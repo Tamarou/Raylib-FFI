3b62d6f3f2049b25f83e9bce119fa2a3bc900125
Chris Prather <chris@prather.org>
2024-05-17 13:42:38 -0400

    add synopsis to Raylib::FFI itself

--- lib/Raylib/FFI.pm
+++ lib/Raylib/FFI.pm
@@ -89,9 +89,32 @@ __END__
 
 Raylib::FFI - Perl FFI bindings for raylib
 
+=head1 SYNOPSIS
+
+    use 5.38.2;
+    use lib qw(lib);
+    use Raylib::FFI;
+    use constant Color => 'Raylib::FFI::Color';
+
+    InitWindow( 800, 600, "Testing!" );
+    SetTargetFPS(60);
+    while ( !WindowShouldClose() ) {
+        my $x = GetScreenWidth() / 2;
+        my $y = GetScreenHeight() / 2;
+        BeginDrawing();
+        ClearBackground( Color->new( r => 0, g => 0, b => 0, a => 0 ) );
+        DrawFPS( 0, 0 );
+        DrawText( "Hello, world!",
+            $x, $y, 20, Color->new( r => 255, g => 255, b => 255, a => 255 ) );
+        EndDrawing();
+    }
+    CloseWindow();
+
 =head1 DESCRIPTION
 
 This module provides Perl bindings for the raylib library using FFI::Platyus.
+This is functional but very low level. You probably want to use L<Raylib::App>
+instead.
 
 =head1 AUTHOR
 
